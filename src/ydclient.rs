a2b64405bcbc9cb99efcd96e17bee9f7416cfaa8
kvarenzn <12197126+kvarenzn@users.noreply.github.com>
2022-07-20 21:25:25 +0800

    docking new translate_o api

--- src/ydclient.rs
+++ src/ydclient.rs
@@ -1,23 +1,29 @@
 //! ydclient is client wrapper for Client
 
 use serde_json::{self, Error as SerdeError};
+use std::collections::HashMap;
 use std::env::var;
 use std::error::Error;
 // use reqwest::header::Connection;
 use super::ydresponse::YdResponse;
 use crate::Client;
+use reqwest::header::{COOKIE, HOST, ORIGIN, REFERER, USER_AGENT};
 use reqwest::Url;
+
+use hex;
+use md5::{Digest, Md5};
+use std::time::{SystemTime, UNIX_EPOCH};
 
 lazy_static! {
     /// API name
     static ref API: String = var("YDCV_API_NAME")
         .unwrap_or_else(|_| var("YDCV_YOUDAO_APPID")
-        .unwrap_or_else(|_| String::from("ydcv-rs")));
+        .unwrap_or_else(|_| String::from("fanyideskweb")));
 
     /// API key
     static ref API_KEY: String = var("YDCV_API_KEY")
         .unwrap_or_else(|_| var("YDCV_YOUDAO_APPSEC")
-        .unwrap_or_else(|_| String::from("1323298384")));
+        .unwrap_or_else(|_| String::from("Ygy_4c=r#e#4EX^NUGUc5")));
 }
 
 /// Wrapper trait on `reqwest::Client`
@@ -52,21 +58,60 @@ impl YdClient for Client {
     fn lookup_word(&mut self, word: &str, raw: bool) -> Result<YdResponse, Box<dyn Error>> {
         use std::io::Read;
 
-        let mut url = Url::parse("https://fanyi.youdao.com/openapi.do")?;
-        url.query_pairs_mut().extend_pairs(
-            [
-                ("keyfrom", API.as_str()),
-                ("key", API_KEY.as_str()),
-                ("type", "data"),
-                ("doctype", "json"),
-                ("version", "1.1"),
-                ("q", word),
-            ]
-            .iter(),
-        );
+        let mut url = Url::parse("https://fanyi.youdao.com/translate_o")?;
+        url.query_pairs_mut()
+            .extend_pairs([("smartresult", "dict"), ("smartresult", "rule")].iter());
+
+        let api = API.clone();
+        let api_key = API_KEY.clone();
+
+        let mut params = HashMap::new();
+        params.insert("i", word);
+        params.insert("from", "AUTO");
+        params.insert("to", "AUTO");
+        params.insert("smartresult", "dict");
+        params.insert("client", &api);
+        params.insert("doctype", "json");
+        params.insert("version", "2.1");
+        params.insert("keyfrom", "fanyi.web");
+        params.insert("action", "FY_BY_DEFAULT");
+
+        let app_version = String::from("5.0 (X11)");
+        let mut hasher = Md5::new();
+        hasher.update(app_version.as_bytes());
+        let bv = hex::encode(hasher.finalize());
+        params.insert("bv", &bv);
+
+        let timestamp = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_millis();
+        let timestamp = timestamp.to_string();
+        params.insert("lts", &timestamp);
+
+        params.insert("salt", &timestamp);
+
+        let sign = format!("{}{}{}{}", api, word, timestamp, api_key);
+        let mut hasher = Md5::new();
+        hasher.update(sign.as_bytes());
+        let signed = hex::encode(hasher.finalize());
+        params.insert("sign", &signed);
+
         let mut body = String::new();
-        self.get(url)
-            // .header(Connection::close())
+        self.post(url)
+            .header(
+                USER_AGENT,
+                "Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0",
+            )
+            .header(HOST, "fanyi.youdao.com")
+            .header(ORIGIN, "https://fanyi.youdao.com")
+            .header(REFERER, "https://fanyi.youdao.com/")
+            .header(COOKIE, "OUTFOX_SEARCH_USER_ID=0@0.0.0.0")
+            .header("X-Requested-With", "XMLHttpRequest")
+            .header("Sec-Fetch-Dest", "empty")
+            .header("Sec-Fetch-Mode", "cors")
+            .header("Sec-Fetch-Site", "same-origin")
+            .form(&params)
             .send()?
             .read_to_string(&mut body)?;
 
@@ -76,35 +121,5 @@ impl YdClient for Client {
         } else {
             self.decode_result(&body).map_err(Into::into)
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use Client;
-
-    #[test]
-    fn test_lookup_word_0() {
-        assert_eq!(
-            "YdResponse('hello')",
-            format!("{}", Client::new().lookup_word("hello", false).unwrap())
-        );
-    }
-
-    #[test]
-    fn test_lookup_word_1() {
-        assert_eq!(
-            "YdResponse('world')",
-            format!("{}", Client::new().lookup_word("world", false).unwrap())
-        );
-    }
-
-    #[test]
-    fn test_lookup_word_2() {
-        assert_eq!(
-            "YdResponse('<+*>?_')",
-            format!("{}", Client::new().lookup_word("<+*>?_", false).unwrap())
-        );
     }
 }
--- src/ydresponse.rs
+++ src/ydresponse.rs
@@ -5,29 +5,28 @@ use serde_json::{self, Error as SerdeError};
 
 /// Basic result structure
 #[derive(Serialize, Deserialize, Debug)]
-pub struct YdBasic {
-    explains: Vec<String>,
-    phonetic: Option<String>,
-    us_phonetic: Option<String>,
-    uk_phonetic: Option<String>,
+pub struct YdTranslateResult {
+    #[serde(rename = "tgt")]
+    target: String,
+    #[serde(rename = "src")]
+    source: String,
 }
 
 /// Web result structure
 #[derive(Serialize, Deserialize, Debug)]
-pub struct YdWeb {
-    key: String,
-    value: Vec<String>,
+pub struct YdSmartResult {
+    entries: Vec<String>,
+    #[serde(rename = "type")]
+    typ: i32,
 }
 
 /// Full response structure
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct YdResponse {
-    query: String,
     error_code: i32,
-    translation: Option<Vec<String>>,
-    basic: Option<YdBasic>,
-    web: Option<Vec<YdWeb>>,
+    translate_result: Option<Vec<Vec<YdTranslateResult>>>,
+    smart_result: Option<YdSmartResult>,
 }
 
 impl YdResponse {
@@ -39,90 +38,32 @@ impl YdResponse {
     pub fn explain(&self, fmt: &dyn Formatter) -> String {
         let mut result: Vec<String> = vec![];
 
-        if self.error_code != 0
-            || self.basic.is_none() && self.web.is_none() && self.translation.is_none()
-        {
+        if self.error_code != 0 || self.translate_result.is_none() {
             result.push(fmt.red(" -- No result for this query."));
             return result.join("\n");
         }
 
-        if self.basic.is_none() && self.web.is_none() {
-            result.push(fmt.underline(&self.query));
-            result.push(fmt.cyan("  Translation:"));
-            result.push("    ".to_owned() + &self.translation.as_ref().unwrap().join("；"));
-            return result.join("\n");
+        result.push(fmt.cyan("  翻译结果："));
+
+        if let Some(ref translate_result) = self.translate_result {
+            for paragraph in translate_result {
+                for sentense in paragraph {
+                    result.push(format!(
+                        "    {}\n    {}",
+                        fmt.underline(&sentense.source),
+                        sentense.target.clone()
+                    ));
+                }
+            }
         }
 
-        let phonetic = if let Some(ref basic) = self.basic {
-            if let (Some(us_phonetic), Some(uk_phonetic)) =
-                (basic.us_phonetic.as_ref(), basic.uk_phonetic.as_ref())
-            {
-                format!(
-                    " UK: [{}], US: [{}]",
-                    fmt.yellow(uk_phonetic),
-                    fmt.yellow(us_phonetic)
-                )
-            } else if let Some(ref phonetic) = basic.phonetic {
-                format!("[{}]", fmt.yellow(phonetic))
-            } else {
-                "".to_owned()
-            }
+        if let Some(ref smart_result) = self.smart_result {
+            result.push(fmt.yellow("\n  智能结果："));
+            result.push(smart_result.entries.join("    "));
         } else {
-            "".to_owned()
-        };
-
-        result.push(format!(
-            "{} {} {}",
-            fmt.underline(&self.query),
-            phonetic,
-            fmt.default(
-                &self
-                    .translation
-                    .as_ref()
-                    .map(|v| v.join("; "))
-                    .unwrap_or_default()
-            )
-        ));
-
-        if let Some(ref basic) = self.basic {
-            if !basic.explains.is_empty() {
-                result.push(fmt.cyan("  Word Explanation:"));
-                for exp in &basic.explains {
-                    result.push(fmt.default(&("     * ".to_owned() + exp)));
-                }
-            }
-        }
-
-        if let Some(ref web) = self.web {
-            if !web.is_empty() {
-                result.push(fmt.cyan("  Web Reference:"));
-                for item in web {
-                    result.push("     * ".to_owned() + &fmt.yellow(&item.key));
-                    result.push(
-                        "       ".to_owned()
-                            + &item
-                                .value
-                                .iter()
-                                .map(|x| fmt.purple(x))
-                                .collect::<Vec<_>>()
-                                .join("；"),
-                    );
-                }
-            }
+            result.push(String::new());
         }
 
         result.join("\n")
-    }
-}
-
-// For testing
-
-#[cfg(test)]
-use std::fmt;
-
-#[cfg(test)]
-impl fmt::Display for YdResponse {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "YdResponse('{}')", self.query)
     }
 }
